2e6d5ba36b5094cf30e9790c6ba39608fbb61731
MhrjAniiz <anishmaharjan17@gmail.com>
2020-11-02 18:47:17 +0545

    made directory and css

---
+++ src/pages/Homepage/HomePage.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./HomePage.styles.scss";
+import DirectoryComponent from "../../components/directory/DirectoryComponent";
+
+const HomePage = () => (
+  <div className="homepage">
+    <DirectoryComponent />
+  </div>
+);
+
+export default HomePage;
